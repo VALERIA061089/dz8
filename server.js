b7e585cf478332c4eb865a4e7597a04519ec3eaa
VALERIA061089 <val2269@yandex.ru>
2024-09-27 15:43:09 +0300

    git init

---
+++ server.js
@@ -0,0 +1,8 @@
+const { add, subtract } = require('./math.js');
+
+
+const sum = add(5, 3);
+const difference = subtract(5, 3);
+
+console.log(`Сумма: ${sum}`); 
+console.log(`Разность: ${difference}`); 
